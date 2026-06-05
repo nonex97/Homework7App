2c2ef7165cbaab0e94d2aee9afd607dec3433fec
nonex97 <ngrguric97@gmail.com>
2026-06-05 22:11:41 +0200

    App that asks names of students and adds them to a list. When there are no more names it prints out how many students there are.

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -1,4 +1,21 @@
-﻿
+﻿// Add students to a class roster List until there are no more students.
+// Then print out the count of the student to the console.
 
+List<String> firstNames = new List<String>();
+string firstName;
+bool quit = false;
 
-
+do
+{
+    Console.Write("Enter the name of a student (or type quit): ");
+    firstName = Console.ReadLine();
+    if (firstName.ToLower() == "quit")
+    {
+        Console.WriteLine(firstNames.Count() + 1);
+        return;
+    }
+    else
+    {
+        firstNames.Add(firstName);
+    }
+} while (quit == false);
